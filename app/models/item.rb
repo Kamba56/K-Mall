e386f76f7cdfc4a0f930cd571eab5a1431de18ab
Kamba56 <tomajohn316@yahoo.com>
2023-01-05 20:10:55 +0100

    Establish assoiations

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,6 @@
 class Item < ApplicationRecord
+  belongs_to :user
+
+  has_many :store_items
+  has_many :stores, through: :store_items
 end
--- app/models/store.rb
+++ app/models/store.rb
@@ -1,2 +1,6 @@
 class Store < ApplicationRecord
+  belongs_to :user
+
+  has_many :store_items
+  has_many :items, through: :store_items
 end
